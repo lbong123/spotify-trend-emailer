d77415f9c2457370109768d304330df9aba17c4b
lbong123 <108723117+lbong123@users.noreply.github.com>
2022-07-30 00:20:37 +1000

    Update cred.py

--- cred.py
+++ cred.py
@@ -1,7 +1,7 @@
-client_ID="986b15d9230a486bbe93cdcb9f1ca0a9"
-client_SECRET='4fbcd466286e47afad75baf13a58b2fe'   
+client_ID="YOUR_CLIENT_ID"
+client_SECRET='YOUR_CLIENT_SECERET'   
 redirect_url='http://localhost:9000'
 
-EMAIL_ADDRESS = 'spotifywebscraper@gmail.com'
-EMAIL_PASSWORD = 'kxvehiizmkmhymzm'
-TARGET_ADDRESS = "lbong@outlook.com"
+EMAIL_ADDRESS = 'YOUR_EMAIL_ADDRESS'
+EMAIL_PASSWORD = 'YOUR_EMAIL_PASS'
+TARGET_ADDRESS = "TARGET_EMAIL"
